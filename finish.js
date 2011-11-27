ee5f2b37ba4370afe96cfd8c112a5df735b7c93c
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-11-27 19:33:04 +0100

    reading from actual data

--- finish.js
+++ finish.js
@@ -1,14 +1,66 @@
+function saveTasksToLocalStorage (tasks) {
+    $.jStorage.set("Finish", tasks);
+}
 
 function onListPageShown (event) {
-    alert("Jup");
+    Finish = readTasksFromLocalStorage() || Finish;
+    updateWithTasks(Finish);
+}
+
+function updateWithTasks (finish) {
+    updateActiveTask(finish);
+    updateOpenTasks(finish);
+}
+
+function updateActiveTask (finish) {
+    $("#cur-todo-label").text(finish.currentTask);
+}
+
+function updateOpenTasks (finish) {
+    var ul = $("#open-todos-ul");
+    ul.empty();
+    jQuery.each(finish.openTasks.sort(), function(idx, v) {
+        ul.append($("<li>", {text: v}));
+    });
+    ul.listview('refresh');
 }
 
 function readTasksFromLocalStorage () {
+    if ($.jStorage.storageAvailable()) { 
+        return $.jStorage.get("Finish", null);
+    } else {
+        alert("Local Storage wird nicht unterstützt. Tasks können nicht gespeichert werden.");
+        return null;
+    }
+}
+
+// Business
+
+function addTask (finish, task) {
+    finish.openTasks.push(task);
+    return finish;
+}
+
+function closeCurrentTask (finish) {
+    finish.currentTask = null;
+    return finish;
+}
+
+function makeTaskActive (finish, task) {
+    var found = jQuery.inArray(task, finish.openTasks);
+    if (found == -1) { throw "Unbekannte Aufgabe." }
+    finish.currentTask = finish.openTasks[found];
+    finish.openTasks = finish.openTasks.splice(found, 1);
+    return finish;
+}
+
+function isActiveTaskDone (finish) {
+    return finish.currentTask == null;
 }
 
 
 var Finish = {
-    currentTask: "None",
+    currentTask: "Not doing shit",
     openTasks: []
 };
 
